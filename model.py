3d2f2ede599c9de6aa0c52f48ed39fabfdb0809b
Julie Pitman <jpitman1010@gmail.com>
2020-08-14 03:09:58 -0700

    "all pages seem to be working again now.  Login page takes to user-registration page when clicking New User hyperlink,need to check completed login taking to library page"

--- model.py
+++ model.py
@@ -1,12 +1,10 @@
 """Tables for Storybook Creator App and connection to database"""
 import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import Column, Integer, DateTime,String
 
 
 db = SQLAlchemy()
-
-
 
 def connect_to_db(flask_app, db_uri='postgresql:///storybook', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
@@ -22,19 +20,19 @@ class User(db.Model):
     """A user. db.Model is PSQL design tool to help manage, design my database."""
     __tablename__ = "users"
 
-    user_id = db.Column(db.Integer,
+    id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key= True,)
     email = db.Column(db.String, unique = True,)
-    fname = db.Column(db.String,)
-    lname = db.Column(db.String,)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
     password = db.Column(db.String, nullable=False)
 
 
     def __repr__(self):
         """show info about the user"""
 
-        return f"<User ID={self.user_id} Email={self.email}, password ={self.password}, author = {self.fname}{self.lname}>"
+        return f"<User ID={self.id} Email={self.email}, password ={self.password}, author = {self.fname}{self.lname}>"
 
 
 
@@ -48,10 +46,10 @@ class Book(db.Model):
                         primary_key=True,)
     title = db.Column(db.String,)
     # summary = db.Column(db.text,)
-    # genre = db.Column(db.Stringing,)
-    # cover_img = db.Column (db.Stringing)
+    # genre = db.Column(db.String,)
+    # cover_img = db.Column (db.String)
     author_id = db.Column(db.Integer,
-                      db.ForeignKey("users.user_id"),)
+                        db.ForeignKey("users.user_id"),)
     created_date = db.Column(db.DateTime,default=datetime.datetime.utcnow,)
     #change to datetime once seeding is working.
 
@@ -62,7 +60,7 @@ class Book(db.Model):
     def __repr__(self):
         """show info about the book"""
 
-        return f"<User's book = {self.author_id}, Book ID={self.book_id} title={self.title}.>"
+        return f"<User's book = {self.author_id}, Book ID={self.id} title={self.title}.>"
 
 class Page(db.Model):
     """A Book."""
@@ -70,13 +68,13 @@ class Page(db.Model):
 
   
     id= db.Column(db.Integer, 
-                autoincrement=True,
-                primary_key=True,)
+                        autoincrement=True,
+                        primary_key=True,)
     # page_number =db.Column(db.Integer,)
     book_id= db.Column(db.Integer,
-                        db.ForeignKey('books.id'),)
-    text = db.Column(db.Integer,)
-    image = db.Column(db.String,)
+                        db.ForeignKey('books.book_id'),)
+    text = db.Column(db.String)
+    image = db.Column(db.String)
 
 
     book = db.relationship('Book', backref='pages')
@@ -84,7 +82,7 @@ class Page(db.Model):
     def __repr__(self):
         """show info about the pages"""
 
-        return f"< Page Number = {self.id} page text={self.text}, page image = {self.image}.>"
+        return f"< Page Number ={self.id} page text={self.text}, page image = {self.image}.>"
 
     
 
--- storybookcreator.py
+++ storybookcreator.py
@@ -1,30 +1,16 @@
-
 """Server for creating storybooks."""
-from flask import Flask, render_template, request, flash, session, redirect, jsonify
-# from jinja2 import StringictUndefined
-from model import connect_to_db, db, Page,Book,User
-import model
-import crud
-import seed_database
-import os
-import cloudinary
-import cloudinary.uploader 
-import cloudinary.api
+from flask import Flask, render_template, request, flash, session, redirect
 
-# result = cloudinary.uploader.unsigned_upload(file, upload_preset, **options)
+from jinja2 import StrictUndefined
+from model import connect_to_db, db
+import crud
+# from cloudinary import requests, cloudinary.uploader, cloudinary.api
+import os
 
 
 app = Flask(__name__)
 app.secret_key = "dev"
-# app.jinja_env.undefined = StringictUndefined
-
-
-
-cloudinary.config(
-    cloud_name = 'cloud_name',
-    api_key = 'cloudinary_api_key',
-    api_secret = 'cloudinary_api_secret'
-)
+app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/')
@@ -33,9 +19,8 @@ def show_homepage():
     return render_template('/login.html')
 
 
-@app.route('/user_regiStringation', methods = ['POST'])
+@app.route('/register_user', methods = ['POST'])
 def user_reg_post_intake():
-
     email = request.form.get('email')
     password = request.form.get('password')
     fname = request.form.get('fname')
@@ -46,12 +31,13 @@ def user_reg_post_intake():
 
     if email_check:
         flash("A user already exists with that email.  Please try again")
-        return redirect('/user_regiStringation.html')
+        return redirect('/register_user.html')
     else:
-        crud.create_user(email, password, fname, lname)
+        create_user = crud.create_user(email, password, fname, lname)
+        
         return render_template('/library.html')
 
-@app.route('/login', methods = ['POST'])
+@app.route('/', methods = ['POST'])
 def login():
     """Process login"""
     email = request.form.get('email')
@@ -65,20 +51,6 @@ def login():
     else:
         flash("This password didnt match the user login.")
         return redirect('/login.html')
-
-
-
-@app.route('/page_image')
-def add_image_to_page():
-    """adding an image to a book page"""
-    filename = request.files.get('image-upload')
-    print(filename)
-    if filename:
-        response = cloudinary.uploader.upload(filename)
-        print(response)
-        image = response['secure_url']
-        print(image)
-        return render_template('pages.html')
 
 
 
